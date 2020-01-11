3f058bb960441021443e58bfcde8f8b324ff7964
evanandrewson <evanandrewson@gmail.com>
2020-01-10 16:31:49 -0800

    hooked up calcOrderTotal to page

--- lab-07/src/shopping-cart.js
+++ lab-07/src/shopping-cart.js
@@ -1,11 +1,14 @@
 import cart from '../data/cartData.js';
 import crystals from '../data/crystals.js';
 import renderLineItem from './render-line-item.js';
-import { findById } from '../common/utils.js';
+import { findById, calcOrderTotal } from '../common/utils.js';
 
 const root = document.getElementById('root');
+const total = document.getElementById('total');
 
 for(let i = 0; i < cart.length; i++) {
   const element = renderLineItem(cart[i], findById(crystals, cart[i].id));
   root.appendChild(element);
 }
+
+total.textContent = calcOrderTotal(cart, crystals);
